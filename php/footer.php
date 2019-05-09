617b9e0f02181d28d7776e0bfa2643ab82ca1f75
Lukas Nys <lukas.nys2@student.vives.be>
2019-05-09 21:34:58 +0200

    Footer html ingevuld

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,15 @@
+    <footer>
+        <div>
+            2019 - Powered by it-lux
+        </div>
+        <div>
+            <ul>
+                <li>FB</li>
+                <li>LinkedIn</li>
+            </ul>
+        </div>
+    </footer>
 
-    
-</body>
-</html>
+    </body>
+
+    </html>
